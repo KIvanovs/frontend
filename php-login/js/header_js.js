e93b476ceceaa4f63e000b518e4f56444f00e955
KIvanovs <78017664+KIvanovs@users.noreply.github.com>
2023-02-01 23:43:50 +0200

    Delete php-login directory

--- php-login/js/header_js.js
+++
@@ -1,7 +0,0 @@
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
-    navBar= document.querySelector(".header-main-nav");
-    navBar.classList.toggle("active");
-}
-
-
